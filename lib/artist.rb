65fea2325b856b593d3c0798d4eeecb431757cb3
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-12 22:10:32 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor :name
+  
+  
+  def initialize(name)
+    @name = name
+  end
+
+def songs
+  Song.all.select do |song|
+    song.artist == self 
+  end
+end
+  
+  def add_song(song)
+    song.artist = self
+end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+  
+  def song_count
+end
+
+
---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+  attr_accessor :name, :artist
+  @@all = []
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  
+end
